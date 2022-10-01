19a0f1580ea4365cc7cc1db047e5adc7ff8469bb
CliffRobinson <cliff@cogo.co>
2022-10-02 12:25:47 +1300

    finish chapter 2

--- app/lib/pento_web/live/wrong_live.ex
+++ app/lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,15 @@
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: DateTime.utc_now())}
+  def mount(_params, session, socket) do
+    {:ok,
+     assign(
+       socket,
+       score: 0,
+       message: "Make a guess:",
+       time: DateTime.utc_now(),
+       session_id: session["live_socket_id"]
+     )}
   end
 
   def render(assigns) do
@@ -18,6 +25,11 @@ defmodule PentoWeb.WrongLive do
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
+
+      <pre>
+        <%= @current_user.email %>
+        <%= @session_id %>
+      </pre>
     """
   end
 
