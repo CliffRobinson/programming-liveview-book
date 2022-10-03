09857422df2c147f16f138b7be02d0af60214804
CliffRobinson <cliff@cogo.co>
2022-10-03 18:23:01 +1300

    finish chapter 2 challenges

--- app/lib/pento_web/live/user_auth_live.ex
+++ app/lib/pento_web/live/user_auth_live.ex
@@ -1,13 +1,12 @@
 defmodule PentoWeb.UserAuthLive do
   import Phoenix.LiveView
+  import Phoenix.Component
   alias Pento.Accounts
 
   def on_mount(_porque, _params, %{"user_token" => user_token}, socket) do
-    user = Accounts.get_user_by_session_token(user_token)
-
     socket =
       socket
-      |> assign(:current_user, user)
+      |> assign_new(:current_user, fn -> Accounts.get_user_by_session_token(user_token) end)
 
     if socket.assigns.current_user do
       {:cont, socket}
